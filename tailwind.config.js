28c6a2903bf658d073ddc8122afdf86a860b25b8
IVainqueur <ishimvainqueur@gmail.com>
2022-08-30 15:39:06 +0200

    +MUI and tailwind color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     extend: {},
     colors: {
       ...colors,
-      "main": "#"
+      'ek-blue': {
+        DEFAULT: '#3F7CAC',
+        '50': '#3F7CAC',
+        '100': '#376D97',
+        '200': '#28506E',
+        '300': '#193245',
+        '400': '#0A141C',
+      },
     }
   },
   plugins: [],
