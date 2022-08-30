0bfa6e19cedc5ddc7aa97ef848dfbc98c59fe1c9
IVainqueur <ishimvainqueur@gmail.com>
2022-08-30 15:18:40 +0200

    Configuring tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
 module.exports = {
   content: ["./pages/**/*.{html,js,jsx}", "./components/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      "main": "#"
+    }
   },
   plugins: [],
 }
