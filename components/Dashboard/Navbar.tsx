5e7b45f0d355c6b8cbb0be7a5d8518242c49d51c
mugishap <precieuxmugisha@gmail.com>
2022-09-16 23:23:03 +0200

    [feat]:Added remaining teacher pages

--- components/Dashboard/Navbar.tsx
+++ components/Dashboard/Navbar.tsx
@@ -7,7 +7,7 @@ import { BiMenu } from 'react-icons/bi'
 import { FaRegUserCircle } from 'react-icons/fa'
 import { IoLogOutOutline } from 'react-icons/io5'
 import { GrFormClose } from 'react-icons/gr'
-import {IoMdClose} from 'react-icons/io'
+import { IoMdClose } from 'react-icons/io'
 
 export const Navbar = (props: any) => {
   const handleLogout = async () => {
@@ -20,15 +20,15 @@ export const Navbar = (props: any) => {
           props.sideBarActive ?
             <IoMdClose className=' text-white cursor-pointer sm:mr-6 md:mr-8' size={40} onClick={() => { props.setSideBarActive(!props.sideBarActive) }} />
             :
-            <BiMenu className=' text-white cursor-pointer sm:mr-6 md:mr-8'color='white' size={40} onClick={() => { props.setSideBarActive(!props.sideBarActive) }} />
+            <BiMenu className=' text-white cursor-pointer sm:mr-6 md:mr-8' color='white' size={40} onClick={() => { props.setSideBarActive(!props.sideBarActive) }} />
         }
         <a href={'/'} className='cursor-pointer hidden sm:flex'><Image src={logoBig} height={'40px'} alt='' width={'165px'} className='rounded py-[1px] px-[2px] bg-[#0003]' /></a>
         <a href={'/'} className='flex sm:hidden'><Image src={logoSmall} height={'40px'} alt='' width={'60px'} className='rounded py-[1px] px-[2px] bg-[#0003]' /></a>
       </div>
-      <span className='hidden mmsm:flex heading-text text-white text-3xl'>Dashboard</span>
+      <span className='hidden mmsm:flex heading-text text-white text-3xl'>{props.page === 'dashboard' ? 'Dashboard' : props.page === 'marks' ? 'Marks' : props.page === 'students' ? 'Students' : props.page === 'settings' ? 'Settings' : 'Timetables'}</span>
       <div className='flex items-center justify-center'>
         <a className='login-button ' href={'/auth/login'}><button className='btn hidden md:flex login-button hover:grayscale-[50%] text-center items-center justify-center button-text bg-[#4CA7CE] text-white py-[12px] px-[38px] w-[150px] rounded-[2px] text-sm hover:bg-ek-blue-200' style={{ boxShadow: '0px 0px 10px 10px rgb(0 0 0 / 10%)' }}>LOG OUT</button></a>
-        <a className='login-button' href={'/auth/login'}><IoLogOutOutline  className='btn flex md:hidden text-white' size={38} /></a>
+        <a className='login-button' href={'/auth/login'}><IoLogOutOutline className='btn flex md:hidden text-white' size={38} /></a>
       </div>
     </div>
   )
