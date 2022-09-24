6065c6f04fc5bc1b51b45b8c86f9b98869c01535
mugishap <precieuxmugisha@gmail.com>
2022-09-24 21:53:28 +0200

    [feat]:Checking up some stuff

--- pages/teacher/students/upload.tsx
+++ pages/teacher/students/upload.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from '../../../components/Dashboard/Navbar'
 import Sidebar from '../../../components/Dashboard/Sidebar'
 import { ToastContainer } from 'react-toastify'
@@ -18,9 +18,48 @@ const studentsUpload = () => {
         students: [],
         fileName: '',
         timeUploaded: '',
-        isFileUploaded: true,
-        error: ''
+        isFileUploaded: false,
+        errorState: false,
+        errorMessage: ""
     })
+
+    const initApp = () => {
+        const droparea = document.querySelector('.droparea');
+        console.log(droparea);
+
+        const active = () => droparea?.classList.add("border-2 border-green-500 border-dashed");
+
+        const inactive = () => droparea?.classList.remove("border-2 border-green-500 border-dashed");
+
+        const prevents = (e: any) => e.preventDefault();
+
+        ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evtName => {
+            droparea?.addEventListener(evtName, prevents);
+        });
+
+        ['dragenter', 'dragover'].forEach(evtName => {
+            droparea?.addEventListener(evtName, active);
+        });
+
+        ['dragleave', 'drop'].forEach(evtName => {
+            droparea?.addEventListener(evtName, inactive);
+        });
+
+        droparea?.addEventListener("drop", handleDrop);
+
+    }
+
+    useEffect(() => {
+        document?.addEventListener("DOMContentLoaded", initApp);
+    }, [])
+
+    const handleDrop = (e: any) => {
+        const dt = e.dataTransfer;
+        const files = dt.files;
+        const fileArray = [...files];
+        console.log(files); // FileList
+        console.log(fileArray);
+    }
 
     const previewFile = () => {
         const inputElt = document.querySelector('#excelFileToUpload') as HTMLInputElement
@@ -59,7 +98,7 @@ const studentsUpload = () => {
                 }
                 <div className={`${sideBarActive ? 'w-10/12' : 'w-full'} flex flex-col items-center justify-center pt-[60px] h-screen p-10`}>
 
-                    <div className='my-auto h-3/5 w-9/12 flex flex-row items-center justify-center'>
+                    <div className='my-auto h-3/5 w-9/12 flex flex-col sm:flex-row items-center justify-center'>
                         <div className="steps flex sm10:mr-8 sm10:flex-col items-center justify-center">
                             <div onClick={() => { setStep(1) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step === 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
                             <div onClick={() => { setStep(2) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step !== 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
@@ -78,7 +117,7 @@ const studentsUpload = () => {
                                             <span className='text-xl w-11/12'>The first row should be in the following format. And the columns must be arranged accordingly.</span>
                                         </span>
                                     </div>
-                                    <div className='mx-auto my-8 bg-white flex flex-row items-center justify-center w-fit rounded h-fit'>
+                                    <div className='mx-auto lg:scale-90 xl:scale-100 my-8 bg-white flex flex-row items-center justify-center w-fit rounded h-fit'>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>First Name</div>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Last Name</div>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Code / ID</div>
@@ -91,24 +130,30 @@ const studentsUpload = () => {
                                 </div>
                                 :
                                 fileData.isFileUploaded ?
-                                    <div className={`${sideBarActive ? 'w-full' : 'w-11/12'} rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center`}>
-                                        <div className='flex flex-col items-center justify-center'>
-                                            <BiCog size={45} color="#4CA7CE" className='rotating my-4 mx-8' />
-                                            <span>Processing...</span>
+                                    fileData.errorState
+                                        ?
+                                        <div>
+                                            <BiInfoCircle size={45} color="#4CA7CE" className='mx-8' />
+                                            <span className='text-xl'>{fileData.errorMessage}</span>
+                                            <button className='px-6 mt-8 absolute right-12 bottom-6 rounded text-white font-normal py-2 bg-ek-blue-75 cursor-pointer' onClick={() => { setStep(2) }}>GOT IT</button>
                                         </div>
-                                    </div>
+                                        :
+                                        <div className={`${sideBarActive ? 'w-full' : 'w-11/12'} rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center`}>
+                                            <div className='flex flex-col items-center justify-center'>
+                                                <BiCog size={45} color="#4CA7CE" className='rotating my-4 mx-8' />
+                                                <span>Processing...</span>
+                                            </div>
+                                        </div>
                                     :
                                     <div className='droparea w-11/12 rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center'>
                                         <div className='flex flex-col items-center justify-center'>
                                             <input type="file" id='excelFileToUpload' className='hidden' onChange={previewFile} name='excelFileToUpload' />
-                                            <label className='w-full flex items-center justify-center' htmlFor="excelFileToUpload">
+                                            <label className='w-full flex-col flex items-center justify-center' htmlFor="excelFileToUpload">
                                                 <Image width={200} height={100} src={uploadExcel}></Image>
                                                 <p>Drop file here</p>
                                             </label>
-
                                         </div>
                                     </div>
-
                         }
                     </div>
                 </div>
