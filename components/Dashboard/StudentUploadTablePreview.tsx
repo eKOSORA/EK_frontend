f85ac0fe1acb14b46f713ac0f08c11048a4dcdac
mugishap <precieuxmugisha@gmail.com>
2022-09-25 18:56:40 +0200

    [feat]:Added Student Upload Page

---
+++ components/Dashboard/StudentUploadTablePreview.tsx
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import { styled } from '@mui/system';
+import TablePaginationUnstyled, {
+    tablePaginationUnstyledClasses as classes,
+} from '@mui/base/TablePaginationUnstyled';
+
+const Root = styled('div')`
+  table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+  th {
+      border: 1px solid #ddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    th {
+        background-color: #ddd;
+    }
+    `;
+
+const CustomTablePagination = styled(TablePaginationUnstyled)`
+  & .${classes.toolbar} {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    gap: 10px;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+        align-items: center;
+    }
+}
+
+& .${classes.selectLabel} {
+    margin: 0;
+}
+
+& .${classes.displayedRows} {
+    margin: 0;
+
+    @media (min-width: 768px) {
+        margin-left: auto;
+    }
+}
+
+& .${classes.spacer} {
+    display: none;
+}
+
+& .${classes.actions} {
+    display: flex;
+    gap: 0.25rem;
+}
+`;
+
+function StudentUploadTablePreview(props: any) {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const rows = props.fileData.students[0]
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    return (
+        <div className='w-full flex flex-col items-center justify-center'>
+            <h1 className='w-full text-center font-semibold heading-text text-3xl mb-6 text-ek-blue'>Table Preview</h1>
+            <Root sx={{ maxWidth: '100%', width: '100%' }}>
+                <table className='rounded' aria-label="custom pagination table">
+                    <thead className=''>
+                        <tr className='font-questrial bg-ek-blue'>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Code/ID</th>
+                            <th>Year/Grade</th>
+                            <th>Class</th>
+                            <th>Parent Email(s)</th>
+                            <th>Parent Tel(s)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {(rowsPerPage > 0
+                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rows
+                        ).map((row: any) => (
+                            <tr key={row['Code/ID']}>
+                                <td style={{ width: 260 }} align="right">
+                                    {row['First Name']}
+                                </td>
+                                <td style={{ width: 260 }} align="right">
+                                    {row['Last Name']}
+                                </td>
+                                <td style={{ width: 260 }} align="right">
+                                    {row['Code/ID']}
+                                </td>
+                                <td style={{ width: 260 }} align="right">
+                                    {row['Year/Grade']}
+                                </td>
+                                <td style={{ width: 260 }} align="right">
+                                    {row['Class']}
+                                </td>
+                                <td style={{ width: 360 }} align="right">
+                                    {row['Parent Email(s)']}
+                                </td>
+                                <td style={{ width: 360 }} align="right">
+                                    {row['Parent Tel(s)']}
+                                </td>
+                            </tr>
+                        ))}
+                        {emptyRows > 0 && (
+                            <tr style={{ height: 41 * emptyRows }}>
+                                <td colSpan={3} />
+                            </tr>
+                        )}
+                    </tbody>
+                    <tfoot className='w-full'>
+                        <tr className='w-full'>
+                            <CustomTablePagination
+                                className='w-full'
+                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                colSpan={7}
+                                count={rows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                componentsProps={{
+                                    select: {
+                                        'aria-label': 'rows per page',
+                                    },
+                                    actions: {
+                                        showFirstButton: true,
+                                        showLastButton: true,
+                                    } as any,
+                                }}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                        </tr>
+                    </tfoot>
+                </table>
+            </Root>
+        </div>
+    );
+}
+
+export default StudentUploadTablePreview
--- pages/educator/students/upload.tsx
+++ pages/educator/students/upload.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
 import { Navbar } from '../../../components/Dashboard/Navbar'
 import Sidebar from '../../../components/Dashboard/Sidebar'
-import { ToastContainer } from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import 'animate.css'
 import { userTeacher } from '../../../utils/faker'
@@ -10,6 +10,7 @@ import { BiCog, BiInfoCircle } from 'react-icons/bi'
 import uploadExcel from '../../../public/img/uploadExcel.svg'
 import Image from 'next/image'
 import * as XLSX from 'xlsx';
+import StudentUploadTablePreview from '../../../components/Dashboard/StudentUploadTablePreview'
 
 const studentsUpload = () => {
     //Important states
@@ -21,7 +22,8 @@ const studentsUpload = () => {
         timeUploaded: '',
         isFileUploaded: false,
         errorState: false,
-        errorMessage: ""
+        errorMessage: "",
+        loading: false
     })
     useEffect(() => {
     }, [])
@@ -37,7 +39,10 @@ const studentsUpload = () => {
 
     const previewFile = async () => {
         var inputElement = document.querySelector('#excelFileToUpload') as HTMLInputElement;
+        const needed = ['First Name', 'Last Name', 'Code/ID', 'Year/Grade', 'Class', 'Parent Email(s)', 'Parent Tel(s)']
+
         if (inputElement.files) {
+            setFileData({ ...fileData, loading: true })
             var name = inputElement.files[0].name;
             const reader = new FileReader();
             reader.addEventListener('load', (e: any) => { // e = on_file_select event
@@ -47,23 +52,66 @@ const studentsUpload = () => {
                 /* Get first worksheet */
                 const sheetCount = wb.SheetNames.length;
                 if (sheetCount > 1) {
-                    const studentSheetData = []
                     for (let i = 0; i < sheetCount; i++) {
                         const wsname = wb.SheetNames[i];
                         const ws = wb.Sheets[wsname];
-                        /* Convert array of arrays */
-                        const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+                        const data = XLSX.utils.sheet_to_json(ws, {});
+                        if (!data[0]) {
+                            setFileData({ ...fileData, errorState: true, errorMessage: "No data found in the excel file" })
+                            toast.error(`No data found in the sheet called ${wsname} excel file`, {
+                                position: "bottom-center",
+                                autoClose: 5000,
+                                hideProgressBar: true,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                theme: "colored"
+                            })
+                            console.log("No data found");
+                            return
+                        }
+                        const columns = Object.keys(data[0])
+  
+                        fileData.students.push(data)
                         console.log(data);
-                        studentSheetData.push({ sheetName: wb.SheetNames[i], data })
                     }
-                    console.log(studentSheetData);
+                    setFileData({...fileData,isFileUploaded:true,})
                 }
                 else {
                     const wsname = wb.SheetNames[0];
                     const ws = wb.Sheets[wsname];
                     /* Convert array of arrays */
-                    const data = XLSX.utils.sheet_to_csv(ws, {});
-                    /* Update state */
+                    const data = XLSX.utils.sheet_to_json(ws, {});
+                    if (!data[0]) {
+                        setFileData({ ...fileData, errorState: true, errorMessage: "No data found in the excel file" })
+                        toast.error("No data found in the excel file", {
+                            position: "bottom-center",
+                            autoClose: 5000,
+                            hideProgressBar: true,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            theme: "colored"
+                        })
+                        console.log("No data found");
+                        return
+                    }
+                    const columns = Object.keys(data[0])
+                    if (columns !== needed) {
+                        setFileData({ ...fileData, errorState: true, errorMessage: "The excel file has columns in wrong format" })
+                        toast.error("Columns are not in the right order", {
+                            position: "bottom-center",
+                            autoClose: 5000,
+                            hideProgressBar: true,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            theme: "colored"
+                        })
+                        console.log("Columns are not in the right order");
+                        return
+                    }
+                    setFileData({ ...fileData, loading: false, students: data, fileName: name, timeUploaded: new Date().toLocaleString(), isFileUploaded: true })
                     console.log(data);
                 }
             })
@@ -102,10 +150,15 @@ const studentsUpload = () => {
                 <div className={`${sideBarActive ? 'w-10/12' : 'w-full'} flex flex-col items-center justify-center pt-[60px] h-screen p-10`}>
 
                     <div className='my-auto h-3/5 w-9/12 flex flex-col sm:flex-row items-center justify-center'>
-                        <div className="steps flex sm10:mr-8 sm10:flex-col items-center justify-center">
-                            <div onClick={() => { setStep(1) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step === 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
-                            <div onClick={() => { setStep(2) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step !== 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
-                        </div>
+                        {
+                            fileData.isFileUploaded
+                                ?
+                                null
+                                :
+                                <div className="steps flex sm10:mr-8 sm10:flex-col items-center justify-center">
+                                    <div onClick={() => { setStep(1) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step === 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
+                                    <div onClick={() => { setStep(2) }} className={`h-16 w-1 cursor-pointer  my-1 mx-10 sm10:mx-0 -rotate-90 sm10:rotate-0 rounded-[3px] ${step !== 1 ? 'bg-ek-blue' : 'bg-ek-blue/20'}`}></div>
+                                </div>}
                         {
                             step === 1 ?
                                 <div className={`relative ${sideBarActive ? 'w-full' : 'w-11/12'} rounded h-full flex flex-col bg-ek-blue/10  items-start justify-center`}>
@@ -116,15 +169,15 @@ const studentsUpload = () => {
                                     <div className='w-full flex items-start px-28 flex-col justify-start '>
                                         <span className='text-xl text-black font-questrial'>There are some simple changes that you'll have to make on your excel file to make it easier for our system to use.</span>
                                         <span className='flex items-start my-4 justify-start'>
-                                            <input className='mr-4 ' type="checkbox" name="" checked id="" />
+                                            <input onChange={() => { }} className='mr-4 ' type="checkbox" name="" checked id="" />
                                             <span className='text-xl w-11/12'>The first row should be in the following format. And the columns must be arranged accordingly.</span>
                                         </span>
                                     </div>
                                     <div className='mx-auto lg:scale-90 xl:scale-100 my-8 bg-white flex flex-row items-center justify-center w-fit rounded h-fit'>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>First Name</div>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Last Name</div>
-                                        <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Code / ID</div>
-                                        <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Year / Grade</div>
+                                        <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Code/ID</div>
+                                        <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Year/Grade</div>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Class</div>
                                         <div className='text-base text-black px-4 py-1 border-r-2 border-r-ek-blue-50/70 mx-0 '>Parent Email(s)</div>
                                         <div className='text-base text-black px-4 py-1 border-r-ek-blue-50/70 mx-0 '>Parent Tel(s)</div>
@@ -132,31 +185,27 @@ const studentsUpload = () => {
                                     <button className='px-6 mt-8 absolute right-12 bottom-6 rounded text-white font-normal py-2 bg-ek-blue-75 cursor-pointer' onClick={() => { setStep(2) }}>GOT IT</button>
                                 </div>
                                 :
-                                fileData.isFileUploaded ?
-                                    fileData.errorState
-                                        ?
-                                        <div>
-                                            <BiInfoCircle size={45} color="#4CA7CE" className='mx-8' />
-                                            <span className='text-xl'>{fileData.errorMessage}</span>
-                                            <button className='px-6 mt-8 absolute right-12 bottom-6 rounded text-white font-normal py-2 bg-ek-blue-75 cursor-pointer' onClick={() => { setStep(2) }}>GOT IT</button>
-                                        </div>
-                                        :
-                                        <div className={`${sideBarActive ? 'w-full' : 'w-11/12'} rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center`}>
-                                            <div className='flex flex-col items-center justify-center'>
-                                                <BiCog size={45} color="#4CA7CE" className='rotating my-4 mx-8' />
-                                                <span>Processing...</span>
-                                            </div>
-                                        </div>
-                                    :
-                                    <div className='droparea w-11/12 rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center'>
+                                fileData.loading ?
+                                    <div className={`${sideBarActive ? 'w-full' : 'w-11/12'} rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center`}>
                                         <div className='flex flex-col items-center justify-center'>
-                                            <input type="file" id='excelFileToUpload' className='hidden' onChange={previewFile} name='excelFileToUpload' />
-                                            <label className='w-full flex-col flex items-center justify-center' htmlFor="excelFileToUpload" id='excelFileToUploadLabel'>
-                                                <Image width={200} height={100} src={uploadExcel}></Image>
-                                                <p>Drop file here</p>
-                                            </label>
+                                            <BiCog size={45} color="#4CA7CE" className='rotating my-4 mx-8' />
+                                            <span>Processing...</span>
                                         </div>
                                     </div>
+                                    :
+                                    fileData.isFileUploaded
+                                        ?
+                                        <StudentUploadTablePreview fileData={fileData} />
+                                        :
+                                        <div className='droparea w-11/12 rounded h-full flex flex-col bg-ek-blue/10  items-center justify-center'>
+                                            <div className='flex flex-col items-center justify-center'>
+                                                <input type="file" id='excelFileToUpload' className='hidden' onChange={previewFile} name='excelFileToUpload' />
+                                                <label className='w-full flex-col flex items-center justify-center' htmlFor="excelFileToUpload" id='excelFileToUploadLabel'>
+                                                    <Image width={200} height={100} src={uploadExcel}></Image>
+                                                    <p>Drop file here</p>
+                                                </label>
+                                            </div>
+                                        </div>
                         }
                     </div>
                 </div>
@@ -166,3 +215,15 @@ const studentsUpload = () => {
 }
 
 export default studentsUpload
+
+/* fileData.isFileUploaded ?
+fileData.errorState
+?
+<div>
+    <BiInfoCircle size={45} color="#4CA7CE" className='mx-8' />
+    <span className='text-xl'>{fileData.errorMessage}</span>
+    <button className='px-6 mt-8 absolute right-12 bottom-6 rounded text-white font-normal py-2 bg-ek-blue-75 cursor-pointer' onClick={() => { setStep(2) }}>GOT IT</button>
+</div>
+:
+
+*/
