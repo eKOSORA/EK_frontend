db31f59e66f2f886e95f2a1a8068a025e08b33cd
mugishap <precieuxmugisha@gmail.com>
2022-10-08 20:24:04 +0200

    [feat]:Adding report generator

---
+++ pages/educator/reports/generate/[studentId].tsx
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import React, { useState } from 'react'
+import { Navbar } from '../../../../components/Dashboard/Navbar'
+import Sidebar from '../../../../components/Dashboard/Sidebar'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import 'animate.css'
+import { userTeacher } from '../../../../utils/faker'
+
+const generateReport = () => {
+  //Important states
+  const [sideBarActive, setSideBarActive] = useState(false)
+
+  return (
+    <div className='animate__animated animate__fadeInLeft bg-[#f0f0f0] min-h-screen'>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+      <Head>
+        <title>Generate Report | eKOSORA</title>
+
+      </Head>
+      <Navbar page='Generate Report' sideBarActive={sideBarActive} setSideBarActive={setSideBarActive} />
+      <div className='w-full flex h-full items-start justify-start'>
+        {
+          sideBarActive
+            ?
+            <Sidebar user={userTeacher} page='educator' active='dashboard' />
+            :
+            null
+        }
+        <div className={`${sideBarActive ? 'w-10/12' : 'w-full'} flex flex-col items-center justify-start pt-[60px] h-fit p-10`}>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default generateReport
